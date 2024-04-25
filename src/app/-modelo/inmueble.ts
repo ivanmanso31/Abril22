293a3272c2c875ef9d3417674104d58f1c00e74c
ivanmanso31 <thecrazyplayerlol@gmail.com>
2024-04-25 09:17:02 +0200

    actualizacion servicios

---
+++ src/app/-modelo/inmueble.ts
@@ -0,0 +1,6 @@
+export class Inmueble{
+    id:string='';
+    ruta:string='';
+    descripcion:string='';
+    descripcionLarga:string='';
+}
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink} from '@angular/router';
+import { Inmueble } from '../-modelo/inmueble';
+import { InmuebleService } from '../_servicio/inmueble.service';
 
 @Component({
   selector: 'app-inicio',
@@ -8,9 +10,19 @@ import { RouterLink} from '@angular/router';
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
-  inmuebles= [
-    {id:'1',ruta:'assets/images/inmueble1.jpg',descripcion:'Bienvenido a esta encantadora casa familiar ubicada en una pintoresca calle arbolada en el corazón de la ciudad. Con una fachada clásica y un encanto atemporal, esta propiedad ofrece el equilibrio perfecto entre comodidad moderna y elegancia tradicional.'},
-    {id:'2',ruta:'assets/images/inmueble2.jpg',descripcion:'Descubre el epitome del lujo contemporáneo con esta espectacular casa ubicada en una colina con vistas panorámicas que quitan el aliento. Esta propiedad de diseño vanguardista combina la elegancia con la funcionalidad, ofreciendo un estilo de vida sofisticado en un entorno sereno y privado.'}
-  ];
+export class InicioComponent implements OnInit{
+
+  inmuebles:Inmueble[]=[];
+
+  constructor(private InmuebleServicio: InmuebleService){}
+
+  ngOnInit(): void {
+    this.inmuebles = this.InmuebleServicio.obtenerTodos();
+  }
+
+
+  //inmuebles= [
+    //{id:'1',ruta:'assets/images/inmueble1.jpg',descripcion:'Bienvenido a esta encantadora casa familiar ubicada en una pintoresca calle arbolada en el corazón de la ciudad. Con una fachada clásica y un encanto atemporal, esta propiedad ofrece el equilibrio perfecto entre comodidad moderna y elegancia tradicional.'},
+    //{id:'2',ruta:'assets/images/inmueble2.jpg',descripcion:'Descubre el epitome del lujo contemporáneo con esta espectacular casa ubicada en una colina con vistas panorámicas que quitan el aliento. Esta propiedad de diseño vanguardista combina la elegancia con la funcionalidad, ofreciendo un estilo de vida sofisticado en un entorno sereno y privado.'}
+  //];
 }
